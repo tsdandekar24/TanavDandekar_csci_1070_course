2ca31770f3e061a635fb21321f45eccb81ef608a
Tanav Dandekar <tsdandekar24@gmail.com>
2024-10-21 10:17:24 -0500

    week_08_homework.sql

--- week_08_homework.sql
+++ week_08_homework.sql
@@ -5,4 +5,13 @@
 
 SELECT rental_date, payment_date, return_date
 FROM rental
-INNER JOIN payment on rental.customer_id = payment.customer_id
+INNER JOIN payment on rental.customer_id = payment.customer_id;
+
+SELECT city, SUM(amount)
+FROM city
+INNER JOIN address on city.city_id = address.city_id
+INNER JOIN customer on address.address_id = customer.address_id
+INNER JOIN payment on customer.customer_id = payment.customer_id
+WHERE city = 'Saint Louis' OR city = 'Kansas City'
+GROUP BY city;
+
